f0eac1d24a795925a0f8d1a03cf3deb8cb63cd59
Monit-Vaghela <monitvaghela123@gmail.com>
2026-01-02 17:20:05 +0000

    Added Opcode ADD SUB MUL DIV CMP

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -1,12 +1,15 @@
 #include <vm/vm.h>
 #include <stdio.h>
-static void print_code(vm *vm){
-    int i = 0;
-    while(i < vm->code_size){
-        printf("%d -> %02x\n", i, vm->code[i]);
-        i++;
+
+static int arithmatic_underflow(vm *vm){
+    if(vm->stack.top < 1){
+        printf("[VM ERROR / %d] Not enough Operands to perform ADD\n", vm->pc);
+        vm->running = 0;
+        return 1;
     }
+    return 0;
 }
+
 void vm_init(vm *vm, unsigned char *code, int code_size) {
     printf("Initializing virtual machine\n");
     vm->code = code;
@@ -17,7 +20,6 @@ void vm_init(vm *vm, unsigned char *code, int code_size) {
     stack_init(&vm->stack);
     // stack_init(&vm->call_stack);
     // memory_init(&vm->memory);
-    print_code(vm);
 }
 
 void vm_run(vm *vm) {
@@ -46,12 +48,73 @@ void vm_run(vm *vm) {
 
             case DUP:{
                 if(stack_isempty(&vm->stack) != 0){
-                    printf("[VM ERROR] Cannot Duplicate Top of Stack\n");
+                    printf("[VM ERROR / %d] Cannot Duplicate Top of Stack\n", vm->pc);
                     vm->running = 0;
                     break;
                 }
                 int top_value = vm->stack.data[vm->stack.top];
                 stack_push(&vm->stack, top_value);
+                (vm->pc)++;
+                break;
+            }
+
+            case ADD:{
+                if(arithmatic_underflow(vm) != 0) break;
+
+                int b = stack_pop(&vm->stack);
+                int a = stack_pop(&vm->stack);
+                stack_push(&vm->stack, a + b);
+                (vm->pc)++;
+                break;
+            }
+
+            case SUB:{
+                if(arithmatic_underflow(vm) != 0) break;
+
+                int b = stack_pop(&vm->stack);
+                int a = stack_pop(&vm->stack);
+                stack_push(&vm->stack, a - b);
+                (vm->pc)++;
+                break;
+            }
+
+            case MUL:{
+                if(arithmatic_underflow(vm) != 0) break;
+
+                int b = stack_pop(&vm->stack);
+                int a = stack_pop(&vm->stack);
+                stack_push(&vm->stack, a * b);
+                (vm->pc)++;
+                break;
+            }
+
+            case DIV:{
+                if(arithmatic_underflow(vm) != 0) break;
+
+                int b = stack_pop(&vm->stack);
+                if(b == 0){
+                    printf("[VM Error / %d] Denominator cannot be 0\n", vm->pc);
+                    vm->running = 0;
+                    break;
+                }
+
+                int a = stack_pop(&vm->stack);
+                stack_push(&vm->stack, a / b);
+                (vm->pc)++;
+                break;
+            }
+
+            case CMP:{
+                if(arithmatic_underflow(vm) != 0) break;
+            
+                int b = stack_pop(&vm->stack);
+                int a = stack_pop(&vm->stack);
+
+                if(a < b){
+                    stack_push(&vm->stack, 1);
+                }else{
+                    stack_push(&vm->stack, 0);
+                }
                 (vm->pc)++;
                 break;
             }
@@ -62,7 +125,7 @@ void vm_run(vm *vm) {
             }
 
             default:
-                printf("Invalid opcode %02x at PC %d\n", opcode, vm->pc);
+                printf("[VM Error / %d]Invalid opcode %02x\n", vm->pc, opcode);
                 vm->running = 0;
                 break;
             }
